9a497ac8e4f5f1c713b5822a81713052a1732867
shahzad <shazad.02@gmail.com>
2019-08-20 11:31:22 +0500

    show modal bottom sheet

--- lib/floating_button.dart
+++ lib/floating_button.dart
@@ -5,6 +5,27 @@ class floating_button extends StatefulWidget {
 }
 
 class floating_button_state extends State<floating_button>{
+
+  _showDrawer() {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              child: ListView(
+                children: [
+                  Text('afds'),
+                  Text('afds2'),
+                  Text('afds3'),
+                  Text('afds4'),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(onPressed: null,
@@ -15,11 +36,14 @@ class floating_button_state extends State<floating_button>{
             decoration: BoxDecoration(
                 boxShadow: [BoxShadow(color: Colors.grey,spreadRadius: 5,blurRadius: 5,offset: Offset(5.0, 5.0))]),
             child: InkWell(
-              onTap: (){Navigator.pushNamed(context, '/filter');},
+              //onTap: (){Navigator.pushNamed(context, '/filter');},
+              onTap: (){_showDrawer();},
                 child: Image.asset("assets/filters2.png",scale: 10,)),
           ),
         ),
       ),backgroundColor: Color(0xffffff),);
   }
+
+
 
 }
